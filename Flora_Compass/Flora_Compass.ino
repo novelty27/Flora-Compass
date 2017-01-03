c51f5620f1e8aed8fb6be393d62602f3ac460faa
emc2443 <evanmcarr@gmail.com>
2017-01-03 08:24:59 -0600

    Stripped down the Flora_Compass.ino file to remove all unnecessary code.

--- Flora_Compass/Flora_Compass.ino
+++ Flora_Compass/Flora_Compass.ino
@@ -146,7 +146,6 @@ void configureSensor(void)
 void setup(void) 
 {
 //  while (!Serial);  // wait for flora/leonardo
-      Serial.println("Hit");
   Serial.begin(9600);
   Serial.println(F("LSM9DS0 9DOF Sensor Test")); Serial.println("");
 
@@ -178,53 +177,25 @@ void setup(void)
 /**************************************************************************/
 void loop(void) 
 {  
-//     colorWipe(strip.Color(0,0,0),0);
   int topRing[16]    = { 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0, 15 };
   int bottomRing[16] = { 30, 31, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
 
   /* Get a new sensor event */ 
   sensors_event_t accel, mag, gyro, temp;
-
-//  lsm.getEvent(&accel, &mag, &gyro, &temp); 
-  lsm.getEvent(0, &mag, 0, 0); 
-//
-//  // print out accelleration data
-//  Serial.print("Accel X: "); Serial.print(accel.acceleration.x); Serial.print(" ");
-//  Serial.print("  \tY: "); Serial.print(accel.acceleration.y);       Serial.print(" ");
-//  Serial.print("  \tZ: "); Serial.print(accel.acceleration.z);     Serial.println("  \tm/s^2");
-
-//   print out magnetometer data
-
-  Serial.print("Magn. X: "); Serial.print(mag.magnetic.x);     Serial.print(" ");
-  Serial.print("  \tY: ");   Serial.print(mag.magnetic.y);     Serial.print(" ");
-  Serial.print("  \tZ: ");   Serial.print(mag.magnetic.z);     Serial.print("  \tgauss\n");
-//  Serial.print(mag.magnetic.x);     Serial.print("\t");
-//  Serial.print(mag.magnetic.y);     Serial.print("\t");
-//  Serial.print(mag.magnetic.z);     Serial.print("\n");
-//  Serial.print("MaxX: "); Serial.print(maxX); Serial.print(" MinX: "); Serial.print(minX);
-//  Serial.print(" MaxY: "); Serial.print(maxY); Serial.print(" MinY: "); Serial.print(minY);
-//  Serial.print(" MaxZ: "); Serial.print(maxZ); Serial.print(" MinZ: "); Serial.print(minZ);
-//    
-//  Serial.print("\n\n\n\n\n");
-
   
-//  // print out gyroscopic data
-//  Serial.print("Gyro  X: "); Serial.print(gyro.gyro.x); Serial.print(" ");
-//  Serial.print("  \tY: "); Serial.print(gyro.gyro.y);       Serial.print(" ");
-//  Serial.print("  \tZ: "); Serial.print(gyro.gyro.z);     Serial.println("  \tdps");
-//
-//  // print out temperature data
-//  Serial.print("Temp: "); Serial.print(temp.temperature); Serial.println(" *C");
-//
-//  float maxX = 0.29;
-//  float minX = -0.77;
-//  float midX = (maxX + minX)/2.0;
-//  float maxY = 0.99;
-//  float minY = -0.12;
-//  float midY = (maxY + minY)/2.0;
-//  float maxZ = 0.49;
-//  float minZ = -0.52;
-//  float midZ = (maxZ + minZ)/2.0;
+  lsm.getEvent(0, &mag, 0, 0); 
+  
+//   print out magnetometer data
+  Serial.print(mag.magnetic.x);     Serial.print("\t");
+  Serial.print(mag.magnetic.y);     Serial.print("\t");
+  Serial.print(mag.magnetic.z);     Serial.print("\n");
+
+  float maxX = 0.29;
+  float minX = -0.77;
+  float maxY = 0.99;
+  float minY = -0.12;
+  float maxZ = 0.49;
+  float minZ = -0.52;
 
   strip.setBrightness(10);
   
@@ -235,59 +206,11 @@ void loop(void)
   int bottomGreen = 0;
   int topBlue = 0;
   int bottomBlue = 0;
-
-  float trueX = 0;
-  float trueY = .45;
-  float trueZ = -.41;
-
-  float xTolerance = abs(.1);
-  float yTolerance = abs(.1);
-  float zTolerance = abs(.1);
-  
-
-  //(x <= -.5) means the y axis is pointing up and x axis is parallel to the ground. The bottom of the chip is pointing NE
-  if(isWithinTolerance(trueX, xTolerance, mag.magnetic.x)) {
-    topRed = 255;
-    bottomRed = 255;
-  }
-  else{
-    topRed = 0;
-    bottomRed = 0;      
-  }
-
-  // (y >= .7) means the y axis in the east/west plane and x axis pointing north, bottom of chip pointing east
-  if(isWithinTolerance(trueY, yTolerance, mag.magnetic.y)) {
-    topGreen = 255;
-    bottomGreen = 255;
-  }
-  else {
-    topGreen = 0;
-    bottomGreen = 0;      
-  }
-
-  if(isWithinTolerance(trueZ, zTolerance, mag.magnetic.z)) {
-    topBlue = 255;
-    bottomBlue = 255;
-  } 
-  else {
-    topBlue = 0;
-    bottomBlue = 0;
-  }
-  
   
   uint32_t topColor = strip.Color(topRed, topGreen, topBlue);
   uint32_t bottomColor = strip.Color(bottomRed, bottomGreen, bottomBlue);
-    
-  for(int i = 0; i < 2*sizeof(topRing)/sizeof(topRing[0]); i++) {
-    //need to apply color to top and bottom ring
-    if(i < sizeof(topRing)/sizeof(topRing[0])){
-      strip.setPixelColor(i, topColor);
-    }
-    else {
-      strip.setPixelColor(i, bottomColor);
-    }
-  }
-//  strip.setPixelColor(0, strip.Color(255,255,255));
+
+  strip.setPixelColor(0, strip.Color(255,255,255));
   strip.show();
   delay(25);
 }
@@ -305,73 +228,4 @@ void setStripColor(uint32_t c, uint8_t wait) {
   strip.show();
 }
 
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);   
-    //return strip.Color(255 - WheelPos * 3, 0, 0);
-  } 
-  else if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-    //return strip.Color(0, WheelPos * 3, 0);
-  } 
-  else {
-    WheelPos -= 170;
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<1256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
 
